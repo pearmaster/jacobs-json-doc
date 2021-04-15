8241e8b0ef77c727dd17c01f79b6b6b6e30e9ee0
Jacob Brunson <balena@jacobbrunson.com>
2021-04-15 08:04:42 -0600

    Don't fail when _version is used from setup

--- jacobsjsondoc/__init__.py
+++ jacobsjsondoc/__init__.py
@@ -1,4 +1,9 @@
-from .document import Document
+try:
+    from .document import Document
+except ModuleNotFoundError:
+    import sys
+    if 'setup' not in sys.modules['__main__'].__file__:
+        raise
 from . import _version
 
 __version__ = _version.__version__
