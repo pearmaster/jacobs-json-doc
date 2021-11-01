ddf4991c4bdcf3f178e610c51be3b7b89834fcde
Jacob Brunson <github@jacobbrunson.com>
2021-11-01 00:28:39 +0000

    Try to handle case where JSON has tabs

--- jacobsjsondoc/parser.py
+++ jacobsjsondoc/parser.py
@@ -1,4 +1,5 @@
 from ruamel.yaml import YAML
+from ruamel.yaml.scanner import ScannerError
 
 class Parser(object):
 
@@ -6,7 +7,11 @@ class Parser(object):
         self._yaml = YAML(typ='rt')
 
     def parse_yaml(self, data: str):
-        structure = self._yaml.load(data)
+        try:
+            structure = self._yaml.load(data)
+        except ScannerError as e:
+            fixed_data = data.replace("\t", "    ")
+            structure = self._yaml.load(fixed_data)
         return structure
 
     def parse_json(self, data: str):
